cb48c420b8202591e4c0d660ad8671360cafa432
yanglichuan <yanglichuancom@163.com>
2015-12-04 17:26:24 +0800

    swityJsonDemo

---
+++ myswiftyJson/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  myswiftyJson
+//
+//  Created by  yanglc on 15/12/4.
+//  Copyright © 2015年  yanglc. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let string: NSString = "[{\"user\":\"ylc\", \"name\":\"bbbb\"}]"
+        let data = string.dataUsingEncoding(NSUTF8StringEncoding)
+        let ccc = JSON(data: data!);
+        if let userName = ccc[0]["user"].string {
+            
+            print(userName)
+        }
+        
+                
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
